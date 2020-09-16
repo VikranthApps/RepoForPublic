096e080f036e03a013a98b81c3e7a68d8629acaf
Vikranth Apps <imakingapps@gmail.com>
2020-09-16 20:27:28 +0530

    new for public

---
+++ XcodeFramework.framework/Headers/XcodeFramework.h
@@ -0,0 +1,19 @@
+//
+//  XcodeFramework.h
+//  XcodeFramework
+//
+//  Created by Vikranth Naidu on 16/09/20.
+//  Copyright © 2020 Vikranth Naidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XcodeFramework.
+FOUNDATION_EXPORT double XcodeFrameworkVersionNumber;
+
+//! Project version string for XcodeFramework.
+FOUNDATION_EXPORT const unsigned char XcodeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XcodeFramework/PublicHeader.h>
+
+
